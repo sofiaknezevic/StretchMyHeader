72dfc16302c8506af1f007dc8f6b50d4ee01ebb5
sofiaknezevic <sofia.knezevic@gmail.com>
2017-03-14 19:31:41 -0700

    Took out unnecessary code

--- StretchMyHeader/ViewController.swift
+++ StretchMyHeader/ViewController.swift
@@ -78,22 +78,22 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let newsItems = NSMutableArray()
         
         newsItems.add(NewsItem.init(category: NewsItem.NewsCategory.world,
-                                       andStory: "Something about climate change and all this stuff I don't know anymore"))
+                                       andStory: "Something about climate change and all this stuff I don't know anymore."))
         
         newsItems.add(NewsItem.init(category: NewsItem.NewsCategory.americas,
-                                       andStory: "There are some crazy things happening in North America, and South America and any other america that exists on this planet because america is just a wild wild name"))
+                                       andStory: "There are some crazy things happening in North America, and South America and any other america that exists on this planet because america is just a wild wild name."))
         
         newsItems.add(NewsItem.init(category: NewsItem.NewsCategory.europe,
-                                       andStory: "Well, now this place has got some stuff going on, but like its so beautiful you should definitely still go there if you can"))
+                                       andStory: "Well, now this place has got some stuff going on, but like its so beautiful you should definitely still go there if you can."))
         
         newsItems.add(NewsItem.init(category: NewsItem.NewsCategory.middleEast,
-                                       andStory: "This area of the world is very very hot, and very very dry, did you hear about that camel that bit off its riders head for not giving it enough water? Like, I thought camels could go years without water... I guess that's unreasonable"))
+                                       andStory: "This area of the world is very very hot, and very very dry, did you hear about that camel that bit off its riders head for not giving it enough water? Like, I thought camels could go years without water... I guess that's unreasonable."))
         
         newsItems.add(NewsItem.init(category: NewsItem.NewsCategory.africa,
                                        andStory: "This place is super interesting, I actually found out the other day that there is a tribe in Africa that scarifies their face in the outline of a spiderweb when they are newly-born, its so beautiful but also so crazy!!!"))
         
         newsItems.add(NewsItem.init(category: NewsItem.NewsCategory.asiaPacific,
-                                       andStory: "I don't know much about this area of the world, and I don't like "))
+                                       andStory: "I don't know much about this area of the world, and I don't like that fact about myself."))
         
         return newsItems
         
@@ -131,8 +131,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func updateHeaderView() -> Void
     {
-        let newHeight = kTableHeaderHeight
-        var headerRect = CGRect(x:0, y: -newHeight, width: mainTableView.bounds.width, height: kTableHeaderHeight)
+        
+        var headerRect = CGRect(x:0, y: -kTableHeaderHeight, width: mainTableView.bounds.width, height: kTableHeaderHeight)
 
         if mainTableView.contentOffset.y < -kTableHeaderHeight {
             
